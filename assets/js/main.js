2da6e758881c2198809072b60022daebbff4651a
ilyasisiner <92398458+ilyasisiner@users.noreply.github.com>
2021-12-02 09:37:12 +0300

    Homework done

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,11 +13,11 @@ const showMenu = (toggleId, navId) => {
 showMenu('nav-toggle', 'nav-menu')
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav_menu')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
+    // When we click on each nav_link, we remove the show-menu class
     navMenu.classList.remove('show')
 }
 
